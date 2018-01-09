e84ab6643bf4342246744d1e8ca0c2183af40694
Hardeep Sharma <hardeeps@damcogroup.com>
2018-01-09 11:10:34 +0530

    CHnage view with classView

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.PostView.as_view(), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,67 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, FormView
+from django.views.generic.edit import FormMixin, CreateView
 from .models import Post, Comment
 from django.utils import timezone
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 
-def post_list(request):
-    allposts = Post.objects.order_by('pub_date')
-    paginator = Paginator(allposts, 2, 1) # Show 2 contacts per page 1 is the orphan number
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/post_list.html'
+	paginate_by = 3	
+	
+	def get_context_data(self, **kwargs):
+		context = super(PostListView, self).get_context_data(**kwargs)
+		list_post = Post.objects.all()
+		paginator = Paginator(list_post, self.paginate_by)
+		page = self.request.GET.get('page')
+		try:
+			posts = paginator.page(page)
+		except PageNotAnInteger:
+			posts = paginator.page(1)
+		except EmptyPage:
+			posts = paginator.page(paginator.num_pages)
+			
+		context['posts'] = posts
+		return context
+		
 
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        posts = paginator.page(paginator.num_pages)
+class PostDetailView(FormView, DetailView):
+	model = Post
+	template_name = 'blog/post_detail.html'
+	form_class = CommentForm
+	
+	def get_context_data(self, **kwargs):		
+		context = super(PostDetailView, self).get_context_data(**kwargs)
+		context['form'] = self.get_form()
+		context['comments'] = Comment.objects.filter(post_id=self.get_object().pk).order_by('comment_date')
+		return context
+		
+	def form_valid(self, form):		
+		comment = CommentForm(form.data)
+		if form.is_valid():
+			cForm = form.save(commit=False)
+			cForm.post = self.get_object()
+			cForm.comment_date = timezone.now()
+			cForm.save()
+			return redirect('blog:post_list')
+		
 
-    return render(request, 'blog/post_list.html', {'posts': posts})
-    
-    #posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
-    ##posts = Post.objects.order_by('pub_date')
-    ##return render(request, 'blog/post_list.html', {'posts': posts})
- 
+class PostView(CreateView):
+	model = Post
+	template_name = 'blog/post_edit.html'
+	form_class = PostForm
+	
+	def form_valid(self, form):
+		if form.is_valid():
+			form.pub_date = timezone.now()	
+			form.save()
+			return redirect('blog:post_detail', pk=self.get_object().pk)
+		
+			
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = Comment.objects.filter(post_id=post.pk).order_by('comment_date')
@@ -53,6 +90,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+ 
+ 
     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
